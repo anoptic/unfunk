296dfbe9949ad16b290ffe2e70d6e257d889263f
anoptic <50808486+anoptic@users.noreply.github.com>
2022-11-29 22:14:18 +0100

    cart styled & functioning

--- hooks/useCart.ts
+++ hooks/useCart.ts
@@ -14,28 +14,36 @@ export interface Product {
 type Contents = Product[];
 
 export const contentsAtom = atom([] as Contents);
+export const totalAtom = atom(0);
 
 const useCart = () => {
   const [cartContents, setCartContents] = useAtom(contentsAtom);
+  const [cartTotal, setCartTotal] = useAtom(totalAtom);
   // console.log('hook', cartContents);
 
   const addToCart = (product: Product) => {
     // console.log('add', product);
     product.key = nanoid(8);
     setCartContents([...cartContents, product]);
+    setCartTotal((total) => total + product.price);
+    // const total = cartContents.reduce((a,b) => {
+    //   return a + b.price
+    // }, 0)
   };
 
   const clearCart = () => {
+    setCartTotal(0);
     setCartContents([]);
   };
 
   const removeFromCart = (key: string) => {
+    const removedItem = cartContents.filter((c) => c.key === key);
+    setCartTotal((total) => total - removedItem[0].price);
     const newContents = cartContents.filter((c) => c.key !== key);
     setCartContents(newContents);
   };
 
   return {
-    cartContents,
     addToCart,
     clearCart,
     removeFromCart,
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,54 +1,98 @@
 import { useAtom } from 'jotai';
-import { contentsAtom } from 'hooks/useCart';
+import { contentsAtom, totalAtom } from 'hooks/useCart';
 import useCart from 'hooks/useCart';
 import Link from 'next/link';
-// import { Sizes } from 'pages/shoes/size-widget';
 import styles from './cart.module.css';
-// import CartChip from '@/cart-chip';
-import ImageDisplay from '@/image-display';
+// import ImageDisplay from '@/image-display';
+import Image from 'next/image';
 
 const Cart = () => {
   const { clearCart, removeFromCart } = useCart();
   const [cartContents] = useAtom(contentsAtom);
+  const [cartTotal] = useAtom(totalAtom);
   console.log('cart', cartContents);
 
   return (
-    <>
-      {cartContents.length === 0 ? <p>You cart is empty</p> : null}
-
-      <h2>Your Cart</h2>
-      <div className={styles.cartDisplay}>
-        {cartContents.map((c) => (
-          <div key={c.key} className={styles.item}>
-            <Link href={`/shoes/${c.slug}`}>
-              <div className={styles.image}>
-                <ImageDisplay
-                  source={c.image.fields.file.url}
-                  layout="intrinsic"
-                  height={150}
-                  width={150}
-                />
+    <div className={styles.cartPage}>
+      <div className={styles.header}>
+        <h2 className={styles.pageTitle}>Your Cart</h2>
+        <hr className={styles.rule} />
+      </div>
+      {cartContents.length === 0 ? (
+        <div className={styles.cartDisplay}>
+          <p className={styles.empty}>…is empty. This is unfortunate.</p>
+          <Link href="/all-shoes">
+            <button className="button">Get Shoes</button>
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.cartDisplay}>
+          {cartContents.map((c) => (
+            <div key={c.key} className={styles.itemContainer}>
+              <div className={styles.item}>
+                <Link href={`/shoes/${c.slug}`}>
+                  <div className={styles.image}>
+                    <Image
+                      src={`https:${c.image.fields.file.url}`}
+                      alt={c.name}
+                      layout="responsive"
+                      height={90}
+                      width={90}
+                      sizes="(min-width: 900) 25vh, 15vh"
+                    />
+                  </div>
+                </Link>
+                <div className={styles.rightSide}>
+                  <div className={styles.info}>
+                    <div className={styles.nameSize}>
+                      <Link href={`/shoes/${c.slug}`}>
+                        <div className={styles.name}>{c.name}</div>
+                      </Link>
+                      <div className={styles.size}>Size: {c.size}</div>
+                    </div>
+                    <div className={styles.price}>{c.price}€</div>
+                  </div>
+                  <div>
+                    <button
+                      className={styles.remove}
+                      onClick={() => removeFromCart(c.key!)}
+                    >
+                      <span className={styles.removeBox}>
+                        <span className={styles.removeInner}></span>
+                      </span>
+                    </button>
+                  </div>
+                </div>
               </div>
-            </Link>
-            <div>
-              <Link href={`/shoes/${c.slug}`}>
-                <div>{c.name}</div>
+            </div>
+          ))}
+          <div className={styles.footer}>
+            <hr className={styles.rule} />
+            <div className={styles.total}>
+              <div className={styles.pageTitle}>Cart Total</div>
+              <div className={styles.price}>{cartTotal}€</div>
+            </div>
+            <div className={styles.actions}>
+              <Link href="/">
+                <button className="button">Checkout</button>
               </Link>
-              <div>{c.size}</div>
-              <div>{c.price}</div>
-              <button onClick={() => removeFromCart(c.key!)}>X</button>
+              <button className="button" onClick={clearCart}>
+                Clear Cart
+              </button>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
 
-      <Link href="/">
-        <button className={styles.continue}>Continue</button>
-      </Link>
-      <button className={styles.continue} onClick={clearCart}>
-        Clear Cart
-      </button>
-    </>
+      {/* <div className={styles.actions}>
+        <Link href="/">
+          <button className={styles.continue}>Continue</button>
+        </Link>
+        <button className={styles.continue} onClick={clearCart}>
+          Clear Cart
+        </button>
+      </div> */}
+    </div>
   );
 };
 
