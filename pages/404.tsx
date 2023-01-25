6cd35528b92770f60a19a1cbeb7bedcf3f162d0c
anoptic <50808486+anoptic@users.noreply.github.com>
2023-01-25 14:56:25 +0100

    404 styled

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,36 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import notFound from 'public/assets/not-found.jpg';
+import Glyph404 from './404-glyph';
 import styles from './404.module.css';
 
 const NotFound = () => {
   return (
     <div className={styles.notFound}>
-      <Image
-        src={notFound}
-        alt="bare feet without shoes"
-        layout="fill"
-        objectFit="cover"
-        placeholder="blur"
-        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkKAcAAH0AecUT9IwAAAAASUVORK5CYII="
-      />
+      <div className={styles.background}>
+        <Image
+          src={notFound}
+          alt="bare feet without shoes"
+          className={styles.image}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className={styles.foreground}>
+        <div className={styles.glyph}>
+          <Glyph404 />
+        </div>
+        <div className={styles.text}>
+          <p>There&rsquo;s nothing here.</p>
+          <p>Let&rsquo;s get you someplace</p>
+          <p> more interesting.</p>
+        </div>
+        <div>
+          <Link href="/all-shoes">
+            <button className="button">Get Shoes</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
